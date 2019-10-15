4e40eb315e4c38f3f459ea68b272cdb7db96acea
hapetrosyan <hpetrosyan@questrade.com>
2019-10-15 12:12:15 -0400

    deleting tt.txt

--- test1.py
+++ test1.py
@@ -2,12 +2,16 @@
 import sys
 import data_ops
 import stocksymbol as s
+import matplotlib.pyplot as plt
+
 
 amzn = s.StockSymbol('AMZN')
-df_5_min = amzn.alphavantage_intraday_5_min_df
+# df_5_min = amzn.alphavantage_intraday_5_min_df
+df_1_min = amzn.alphavantage_intraday_1_min_df
+
 # pd = amzn.price_difference
 
-print(df_5_min[[
+print(df_1_min[[
      'datetime'
     ,'open'
     ,'high'
